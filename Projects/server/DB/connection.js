1bd09b838e80f6e5d0d87dbbc089b2c19790002a
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-08-01 13:56:00 +0100

    fixed import issues + modern way of DB connection

--- Projects/server/DB/connection.js
+++ Projects/server/DB/connection.js
@@ -1,11 +1,18 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
 
-const uri = process.env.MONGO_URI
+const uri = process.env.MONGO_URI;
 
+const connectDB = (cb) => {
+  mongoose
+    .connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then((client) => {
+      console.log("connected to mongoDB");
+      return cb();
+    })
+    .catch((err) => {
+      console.log(err);
+      return cb(err);
+    });
+};
 
-const connectDB = () => {
-    connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
-    console.log("connected to mongoDB")
-}
-
-export default connectDB
+export default connectDB;
